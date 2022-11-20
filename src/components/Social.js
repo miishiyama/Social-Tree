715139bdec4d9eb621a8764f73bf6ad53306680b
Millene Ishiyama <milleneeishiyama@gmail.com>
2022-11-20 16:14:35 -0300

    Organizing

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,7 +2,7 @@ export default function Social(props) {
     return (
         <li class="social">
             <a class="social-link" target="_blank" rel="noreferrer" href={props.url}>
-                <img class="social-img" src={props.logo} />
+                <img src={props.logo} />
             </a>
         </li>
     )
